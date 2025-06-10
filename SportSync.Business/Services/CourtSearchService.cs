b62494a96af62fb60496f1757532d499eee01e92
Anhph203 <pham94729@gmail.com>
2025-06-10 10:19:47 +0700

    Fix Search CourtComplex with condition isActiveByOwner is true

--- SportSync.Business/Services/CourtSearchService.cs
+++ SportSync.Business/Services/CourtSearchService.cs
@@ -26,8 +26,8 @@ namespace SportSync.Business.Services
             var complexesQ = _db.CourtComplexes
                 .AsNoTracking()
                 .Include(c => c.Courts)
-                .Where(c => c.ApprovalStatus == ApprovalStatus.Approved &&
-                            c.IsActiveByOwner && c.IsActiveByAdmin);
+                .Where(c => 
+                            c.IsActiveByOwner);
 
             if (!string.IsNullOrWhiteSpace(rq.City))
             {
